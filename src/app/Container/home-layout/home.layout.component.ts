d607cf3038fe4995096fcbd414a4ed8130b309df
shahidaslam0313 <shahid.aslam@brainplow.com>
2019-10-31 17:29:22 +0500

    asdfasdf

--- src/app/Container/home-layout/home.layout.component.ts
+++ src/app/Container/home-layout/home.layout.component.ts
@@ -20,74 +20,74 @@ export class HomeLayoutComponent implements OnInit {
   menuItems = [
     {
       // name: 'Home', route: '/louren', icon: 'fa fa-home', submenu: [
-      name: 'Home', route: '', icon: 'fa fa-home', submenu: [
+      name: 'Home', route: '', icon: '../../../assets/img/sidebar/Home.png', submenu: [
         {
-          name: 'Dashboard', route: '', icon: 'fa fa-tachometer-alt', submenu: [
+          name: 'Dashboard', route: '', icon: '../../../assets/img/sidebar/Dashboard.png', submenu: [
 
-            { name: 'ERCOT Load', route: '/ercot-load', icon: 'fas fa-window-restore', submenu: [] },
+            { name: 'ERCOT Load', route: '/ercot-load', icon: '../../../assets/img/sidebar/ERCOT Load.png', submenu: [] },
             // { name: 'ERCOT Outlook', route: '/ercot-outlook', icon: 'fas fa-clipboard-list', submenu: [] },
             // { name: 'Weather zones', route: '/weather-zones', icon: 'fas fa-cloud-meatball', submenu: [] },
             // { name: 'Prices', route: '/prices', icon: '', submenu: [] },
-            { name: 'Load Graphs', route: '/louren', icon: 'fas fa-chart-line', submenu: [] },
+            { name: 'Load Graphs', route: '/louren', icon: '../../../assets/img/sidebar/Load Graphs.png', submenu: [] },
           ]
         },
        
         
-        { name: 'ESIID Lookup', route: '/esiid-lookup', icon: 'fas fa-id-card-alt', submenu: [] },
+        { name: 'ESIID Lookup', route: '/esiid-lookup', icon: '../../../assets/img/sidebar/ESIID Lookup.png', submenu: [] },
       ]
     },
     {
-      name: 'Dashboard ', route: '/', icon: 'fas fa-poo-storm', submenu: []
+      name: 'Dashboard ', route: '/', icon: '../../../assets/img/sidebar/../../../assets/img/sidebar/Dashboard.png', submenu: []
     },
     {
-      name: 'WattExpert Forecast', route: '', icon: 'fas fa-poo-storm', submenu: [
+      name: 'WattExpert Forecast', route: '', icon: '../../../assets/img/sidebar/WattExpert Al Forecast.png', submenu: [
         // { name: 'WattExpert Al Review Forecast', route: '/forecastertool', icon: 'fab fa-wpforms', submenu: [] },
-        { name: 'Monthly Forecast', route: '/MonthlyForecast', icon: 'fas fa-window-restore', submenu: [], },
-        { name: 'Hourly Forecast', route: '/HourlyForecast', icon: 'fas fa-window-restore', submenu: [] },
-        { name: 'DateRange Forecast', route: '/DateRangeForecast', icon: 'fas fa-window-restore', submenu: [] },
+        { name: 'Monthly Forecast', route: '/MonthlyForecast', icon: '../../../assets/img/sidebar/Monthly.png', submenu: [], },
+        { name: 'Hourly Forecast', route: '/HourlyForecast', icon: '../../../assets/img/sidebar/Hourly.png', submenu: [] },
+        { name: 'DateRange Forecast', route: '/DateRangeForecast', icon: '../../../assets/img/sidebar/review forecaste.png', submenu: [] },
      
-        { name: 'Weather Zone Forecast ', route: '/weather-zone-forecast', icon: 'fas fa-cloud-sun	', submenu: [] },
-        { name: 'Weather Zone Actual', route: '/weather-zone-actual', icon: 'fas fa-cloud-meatball', submenu: [] },
+        { name: 'Weather Zone Forecast ', route: '/weather-zone-forecast', icon: '../../../assets/img/sidebar/weather-zone-forecast.png', submenu: [] },
+        { name: 'Weather Zone Actual', route: '/weather-zone-actual', icon: '../../../assets/img/sidebar/weather-zone-actual.png', submenu: [] },
       ]
     },
     {
-      name: 'Individual Customer Forecast', route: '', icon: 'fas fa-poo-storm', submenu: [
+      name: 'Individual Customer Forecast', route: '', icon: '../../../assets/img/sidebar/maturity-forecast.png', submenu: [
         {
-          name: 'Daily Energy Monitoring  ', route: '/dailyenergy', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Daily Energy Monitoring  ', route: '/dailyenergy', icon: '../../../assets/img/sidebar/Monthly.png', submenu: []
         },
         // {
         //   name: 'Energy Wastage', route: '/energywaste', icon: 'fas fa-poo-storm', submenu: []
         // },
          {
-          name: 'Weekly Comparison', route: '/weekly-comparison', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Weekly Comparison', route: '/weekly-comparison', icon: '../../../assets/img/sidebar/SCR-727 Data Mart.png', submenu: []
         },
     
         {
-          name: 'Energy Spikes', route: '/', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Energy Spikes', route: '/', icon: '../../../assets/img/sidebar/review-esiid-service-history-component.png', submenu: []
         },
         {
-          name: 'Weather Related Events', route: '/', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Weather Related Events', route: '/', icon: '../../../assets/img/sidebar/WattExpert Al Forecast.png', submenu: []
         },
         
       ]
     },
     {
-      name: 'Company Forecast (For ALL Customers)', route: '', icon: 'fas fa-poo-storm', submenu: [
+      name: 'Company Forecast (For ALL Customers)', route: '', icon: '../../../assets/img/sidebar/review-esiid-usage-meter-reads-component.png', submenu: [
         {
-          name: 'Daily Energy Monitoring  ', route: '/dailyenergy', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Daily Energy Monitoring  ', route: '/dailyenergy', icon: '../../../assets/img/sidebar/weather-zone-actual.png', submenu: []
         },
         // {
         //   name: 'Energy Wastage', route: '/energywaste', icon: 'fas fa-poo-storm', submenu: []
         // },
          {
-          name: 'Weekly Comparison', route: '/weekly-comparison', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Weekly Comparison', route: '/weekly-comparison', icon: '../../../assets/img/sidebar/maturity-forecast.png', submenu: []
         },
     
         {
-          name: 'Energy Spikes', route: '/', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Energy Spikes', route: '/', icon: '../../../assets/img/sidebar/review-esiid-usage-meter-reads-component.png', submenu: []
         },
         {
-          name: 'Weather Related Events', route: '/', icon: 'fas fa-poo-storm', submenu: []
+          name: 'Weather Related Events', route: '/', icon: '../../../assets/img/sidebar/WattExpert Al Forecast.png', submenu: []
         },
         
       ]
@@ -95,13 +95,13 @@ export class HomeLayoutComponent implements OnInit {
  
     
     {
-      name: 'SCR-727 Data Mart', route: '', icon: 'fas fa-database', submenu: [
-        { name: 'ESIID Counts', route: '/esiid-count', icon: 'fas fa-clipboard-list', submenu: [] },
-        { name: 'Review ESIID Service History', route: '/review-esiid-service-history-component', icon: '	fas fa-clipboard-list', submenu: [] },
-        { name: 'Review ESIID Usage (Meter Reads)', route: '/review-esiid-usage-meter-reads-component', icon: 'fa fa-clipboard', submenu: [] },
+      name: 'SCR-727 Data Mart', route: '', icon: '../../../assets/img/sidebar/SCR-727 Data Mart.png', submenu: [
+        { name: 'ESIID Counts', route: '/esiid-count', icon: '../../../assets/img/sidebar/ESIID Lookup.png', submenu: [] },
+        { name: 'Review ESIID Service History', route: '/review-esiid-service-history-component', icon: '../../../assets/img/sidebar/esiid-count.png', submenu: [] },
+        { name: 'Review ESIID Usage (Meter Reads)', route: '/review-esiid-usage-meter-reads-component', icon: '../../../assets/img/sidebar/maturity-forecast.png', submenu: [] },
         // { name: 'Review LS Channel Cuts', route: '/review-ls-channel-cuts-component', icon: 'fab fa-wpforms', submenu: [] },
         // { name: 'AMS Interval Data Export', route: '/ams-interval', icon: 'fab fa-wpforms', submenu: [] },
-        { name: 'Review AMS Interval Data ', route: '/review-ams-interval', icon: 'fab fa-wpforms', submenu: [] },
+        { name: 'Review AMS Interval Data ', route: '/review-ams-interval', icon: '../../../assets/img/sidebar/review-ams-interval.png', submenu: [] },
         // { name: 'Usage by ESIID', route: '/', icon: 'fas fa-clipboard-list', submenu: [] },
       ]
     },
@@ -123,10 +123,7 @@ export class HomeLayoutComponent implements OnInit {
   header_name;
   get_local(val1): void{
     this.header_name = val1
-
   }
-  
-   
   ngOnInit() {
  
   //  this.header_name= this.router.url;
